dee9bcd6133488c27e4e9d6e64dc35cfb7a1519e
bryanace86 <bryanace86@gmail.com>
2025-07-18 15:59:01 -0400

    more cleanup

--- OpenAppLab/Core/OpenAppLab.Core.UI.Shared/GraphQL/GraphQLHttpClientService.cs
+++ OpenAppLab/Core/OpenAppLab.Core.UI.Shared/GraphQL/GraphQLHttpClientService.cs
@@ -2,20 +2,8 @@
 using System.Text.Json;
 
 namespace OpenAppLab.Core.UI.Shared.GraphQL;
-public class GraphQLHttpClientService //: IGraphQLHttpClientService
+public class GraphQLHttpClientService
 {
-    //public static async Task<List<T>> ExecuteQueryAsync<T>(string gqlQuery)
-    //{
-    //    var client = new HttpClient();
-    //    var response = await client.PostAsJsonAsync("/graphql", new { query = gqlQuery });
-
-    //    var json = await response.Content.ReadAsStringAsync();
-    //    var parsed = JsonDocument.Parse(json);
-    //    var data = parsed.RootElement.GetProperty("data").GetProperty("posts").GetProperty("items");
-
-    //    return JsonSerializer.Deserialize<List<T>>(data);
-    //}
-
     private readonly HttpClient _httpClient;
     
     public GraphQLHttpClientService(HttpClient httpClient)
